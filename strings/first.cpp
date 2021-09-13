104c1aa9f82990df0262ee71eae08913261fa78c
Ayush khandelwal <ayushkhandelwal519@gmail.com>
2021-09-14 01:29:52 +0530

    day-7

---
+++ strings/first.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    // string str;
+    // cin>>str;
+    // getline(cin, str);
+    // cout << str << endl;
+
+    string s1 = "abcdefghijklu";
+    // s1.erase(3,3);
+    // cout<<s1<<endl;
+
+    // cout<<s1.find("cd")<<endl;
+    
+    s1.insert(2,"lol");
+    cout<<s1<<endl;
+    return 0;
+}
