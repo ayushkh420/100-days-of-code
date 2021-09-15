e413d8c74470b7220ccecccd4eea82a222d0cf29
Ayush khandelwal <ayushkhandelwal519@gmail.com>
2021-09-15 14:02:28 +0530

    day-7

---
+++ strings/second.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main(){
+    string str ="asmifvfnsds";
+    // convert into upper case string
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] -= 32;
+
+        }
+        
+    }
+    cout<<str<<endl;
+
+    // convert into lower case string
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] += 32;         //change
+
+        }
+        
+    }
+    cout<<str<<endl;
+
+    // **USING FUNCTION**
+
+    transform(str.begin(), str.end(),str.begin(), ::toupper);
+    cout<<str<<endl;
+    // lower case
+    transform(str.begin(), str.end(),str.begin(), ::tolower);
+    cout<<str<<endl;
+
+    //!!! sorting in string !!!
+    
+    string s ="452396578";
+    sort(s.begin(), s.end(), greater<int>());
+    cout<<s<<endl;
+    
+    return 0;
+}
